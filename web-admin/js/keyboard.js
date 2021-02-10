e41561902fea85da02183d62ac49b852696f1e94
mayorow <mayorow@gmail.com>
2021-02-10 12:44:08 +0300

    Keyboard events are added in the chat

--- web-admin/js/keyboard.js
+++ web-admin/js/keyboard.js
@@ -1,8 +1,21 @@
 function RemoteKeyboard(remoteChat){
     this.remoteChat = remoteChat;
+	this.manage = false;
+	var obj = this;  // for event handlers
+
+    ui.on('RemoteChat.onManagementStart', function(){
+        this.manage = true;
+    }, this);
+
+    ui.on('RemoteChat.onManagementStop', function(){
+        this.manage = false;
+    }, this);
 
     $(document).on('keydown', function(e){
+		if (!obj.manage) {
+		    return;
+		}
 		var encodedKey = e.key.replace('%', '%25').replace(',', '%2C');
-		this.remoteChat.sendData(`key,${encodedKey}`);
+		obj.remoteChat.sendData(`key,${encodedKey}`);
     });
 }
