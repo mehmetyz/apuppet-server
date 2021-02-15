e085f24e5e2956ffbaef0e8fca487836a42e78f9
Headwind MDM <headwind.mdm@gmail.com>
2021-02-15 11:07:05 +0000

    Control keys are also sent

--- web-admin/js/keyboard.js
+++ web-admin/js/keyboard.js
@@ -12,7 +12,7 @@ function RemoteKeyboard(remoteChat){
     }, this);
 
     $(document).on('keydown', function(e){
-        if (!obj.manage || e.altKey || e.ctrlKey || e.key.length != 1) {
+        if (!obj.manage || e.altKey || e.ctrlKey || !(e.key.length == 1 || e.key === 'Backspace' || e.key === 'Delete' || e.key === 'Enter')) {
             return;
         }
         var encodedKey = e.key.replace('%', '%25').replace(',', '%2C');
